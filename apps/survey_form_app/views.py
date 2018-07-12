93913f8e1ef015e9f41ec529ffa377e9251e6da7
unknown <steelst0@gmail.com>
2018-07-12 10:57:40 -0700

    functional

---
+++ apps/survey_form_app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.
+def index(request):
+    if 'count' not in request.session:
+        request.session['count']=0
+    return render(request,'survey_form_app/index.html')
+
+def result(request):
+    print("WE IN RESULT")
+    return render(request,'survey_form_app/result.html')
+
+def process(request, methods=['POST']):
+    print("WE IN PROCESS")
+    request.session['name'] = request.POST['name']
+    request.session['dojo'] = request.POST['dojo']
+    request.session['language'] = request.POST['language']
+    request.session['comment'] = request.POST['comment']
+    request.session['count']+=1
+    print("NAME= ", request.session['name'])
+    return redirect('/result')
